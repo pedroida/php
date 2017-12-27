2ad0ab12c7c735a181ac5797d63ea7f9f8d22929
pedro <pedrohsida@hotmail.com>
2017-12-27 19:37:11 -0200

    ok

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!<strong> Kappa</strong>";
+
+for ($i=0; $i < 500; $i++) {
+
+	if($i % 2 == 0){ 
+		echo "<br>".$i;
+	}else{
+		echo "<br>batata";
+	}
+}
+
+?>
+
+<script type="text/javascript">
+	confirm("welcome!");
+</script>
